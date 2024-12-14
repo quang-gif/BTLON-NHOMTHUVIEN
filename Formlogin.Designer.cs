37932c6cb796f69b94b86d092a4233069dd42cc6
admin <admin@DESKTOP-5985LPQ>
2024-12-14 17:08:00 +0700

    Add project files.

---
+++ Formlogin.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BTLON_NHOMTHUVIEN
+{
+    partial class Formlogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txttaikhoan = new System.Windows.Forms.TextBox();
+            this.txtmk = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txttaikhoan
+            // 
+            this.txttaikhoan.Location = new System.Drawing.Point(123, 75);
+            this.txttaikhoan.Name = "txttaikhoan";
+            this.txttaikhoan.Size = new System.Drawing.Size(263, 20);
+            this.txttaikhoan.TabIndex = 0;
+            this.txttaikhoan.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtmk
+            // 
+            this.txtmk.Location = new System.Drawing.Point(123, 144);
+            this.txtmk.Name = "txtmk";
+            this.txtmk.Size = new System.Drawing.Size(263, 20);
+            this.txtmk.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tài khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(144, 201);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "Đăng nhập";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(286, 201);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(75, 23);
+            this.btnexit.TabIndex = 2;
+            this.btnexit.Text = "Thoát";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            // 
+            // Formlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 273);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtmk);
+            this.Controls.Add(this.txttaikhoan);
+            this.Name = "Formlogin";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txttaikhoan;
+        private System.Windows.Forms.TextBox txtmk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
