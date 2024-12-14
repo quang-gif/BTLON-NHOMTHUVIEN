a3c2f3a20533330689b46eda830a4fb514f45fb3
Nguyen Anh Dung <Nguyen Anh Dung@SHIBAINU>
2024-12-14 22:12:33 +0700

    ditmequang

--- FormMuonsach.Designer.cs
+++ FormMuonsach.Designer.cs
@@ -28,25 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(245, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(359, 217);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Bấm vào để quang ko bị gay";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormMuonsach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "FormMuonsach";
             this.Text = "Mượn sách";
             this.ResumeLayout(false);
@@ -54,7 +42,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
