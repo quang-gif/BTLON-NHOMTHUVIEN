69ac20894994bebd8a7380f4d780b0550a83b6d4
PhamVanTan74 <tanphanchan@gmail.com>
2024-12-22 18:15:22 +0700

    Đăng bài

--- FormTheloai.Designer.cs
+++ FormTheloai.Designer.cs
@@ -41,6 +41,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCapNhatTheLoai)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,16 +51,18 @@
             this.dgvCapNhatTheLoai.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.matheloai,
             this.tentheloai});
-            this.dgvCapNhatTheLoai.Location = new System.Drawing.Point(18, 145);
-            this.dgvCapNhatTheLoai.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvCapNhatTheLoai.Location = new System.Drawing.Point(24, 178);
+            this.dgvCapNhatTheLoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvCapNhatTheLoai.Name = "dgvCapNhatTheLoai";
             this.dgvCapNhatTheLoai.RowHeadersWidth = 51;
             this.dgvCapNhatTheLoai.RowTemplate.Height = 24;
-            this.dgvCapNhatTheLoai.Size = new System.Drawing.Size(310, 226);
+            this.dgvCapNhatTheLoai.Size = new System.Drawing.Size(413, 278);
             this.dgvCapNhatTheLoai.TabIndex = 21;
+            this.dgvCapNhatTheLoai.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCapNhatTheLoai_CellClick);
             // 
             // matheloai
             // 
+            this.matheloai.DataPropertyName = "matheloai";
             this.matheloai.HeaderText = "Mã thể loại";
             this.matheloai.MinimumWidth = 6;
             this.matheloai.Name = "matheloai";
@@ -67,6 +70,7 @@
             // 
             // tentheloai
             // 
+            this.tentheloai.DataPropertyName = "tentheloai";
             this.tentheloai.HeaderText = "Tên thể loại";
             this.tentheloai.MinimumWidth = 6;
             this.tentheloai.Name = "tentheloai";
@@ -74,50 +78,54 @@
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(347, 223);
-            this.btnThoat.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThoat.Location = new System.Drawing.Point(463, 274);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(56, 19);
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
             this.btnThoat.TabIndex = 20;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click_1);
             // 
             // btnLuu
             // 
-            this.btnLuu.Location = new System.Drawing.Point(347, 183);
-            this.btnLuu.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLuu.Location = new System.Drawing.Point(463, 225);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(56, 19);
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
             this.btnLuu.TabIndex = 19;
             this.btnLuu.Text = "Lưu";
             this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(347, 145);
-            this.btnXoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btnXoa.Location = new System.Drawing.Point(463, 178);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(56, 19);
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
             this.btnXoa.TabIndex = 18;
             this.btnXoa.Text = "Xoá";
             this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnSua
             // 
-            this.btnSua.Location = new System.Drawing.Point(347, 105);
-            this.btnSua.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSua.Location = new System.Drawing.Point(463, 129);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(56, 19);
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
             this.btnSua.TabIndex = 17;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(347, 62);
-            this.btnThem.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThem.Location = new System.Drawing.Point(463, 76);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(56, 19);
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
             this.btnThem.TabIndex = 16;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
@@ -125,28 +133,27 @@
             // 
             // txtTenTheLoai
             // 
-            this.txtTenTheLoai.Location = new System.Drawing.Point(116, 108);
-            this.txtTenTheLoai.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenTheLoai.Location = new System.Drawing.Point(155, 133);
+            this.txtTenTheLoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTenTheLoai.Name = "txtTenTheLoai";
-            this.txtTenTheLoai.Size = new System.Drawing.Size(214, 20);
+            this.txtTenTheLoai.Size = new System.Drawing.Size(284, 22);
             this.txtTenTheLoai.TabIndex = 15;
             // 
             // txtMaTheLoai
             // 
-            this.txtMaTheLoai.Location = new System.Drawing.Point(116, 63);
-            this.txtMaTheLoai.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaTheLoai.Location = new System.Drawing.Point(155, 78);
+            this.txtMaTheLoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMaTheLoai.Name = "txtMaTheLoai";
-            this.txtMaTheLoai.Size = new System.Drawing.Size(214, 20);
+            this.txtMaTheLoai.Size = new System.Drawing.Size(284, 22);
             this.txtMaTheLoai.TabIndex = 14;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(14, 105);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(19, 129);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.Size = new System.Drawing.Size(126, 25);
             this.label3.TabIndex = 13;
             this.label3.Text = "Tên thể loại";
             // 
@@ -154,10 +161,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 62);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(19, 76);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.Size = new System.Drawing.Size(118, 25);
             this.label2.TabIndex = 12;
             this.label2.Text = "Mã thể loại";
             // 
@@ -165,18 +171,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(64, 15);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(85, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 31);
+            this.label1.Size = new System.Drawing.Size(359, 38);
             this.label1.TabIndex = 11;
             this.label1.Text = "CẬP NHẬT THỂ LOẠI";
             // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(463, 313);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 22;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
             // FormTheloai
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 399);
+            this.ClientSize = new System.Drawing.Size(561, 491);
+            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.dgvCapNhatTheLoai);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnLuu);
@@ -188,9 +205,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormTheloai";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thể loại";
+            this.Load += new System.EventHandler(this.FormTheloai_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvCapNhatTheLoai)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -212,5 +231,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridViewTextBoxColumn matheloai;
         private System.Windows.Forms.DataGridViewTextBoxColumn tentheloai;
+        private System.Windows.Forms.Button btnReset;
     }
 }
--- FormTheloai.cs
+++ FormTheloai.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,158 @@ namespace BTLON_NHOMTHUVIEN
 {
     public partial class FormTheloai : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-T6775II7\\SQLEXPRESS;Initial Catalog=DUANNHOMTHUVIEN;Integrated Security=True;Encrypt=False");
         public FormTheloai()
         {
             InitializeComponent();
         }
 
+
+        DataTable tb1;
+        private void load_theloai(string ml, string tl)
+        {
+
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "select * from theloai where " +
+                "matheloai like '%' +@matheloai+ '%' and " +
+                "tentheloai like '%'+@tentheloai+'%' ";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.Add("@matheloai", SqlDbType.NVarChar, 50).Value = ml;
+            cmd.Parameters.Add("@tentheloai", SqlDbType.NVarChar, 100).Value = tl;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            tb1 = new DataTable();
+            da.Fill(tb1);
+            cmd.Dispose();
+            con.Close();
+            dgvCapNhatTheLoai.DataSource = tb1;
+            dgvCapNhatTheLoai.Refresh();
+        }
+        private bool checktrungmatheloai(string ml)
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "select count(*) From theloai where matheloai='" + ml + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            int kq = int.Parse(cmd.ExecuteScalar().ToString());
+            if (kq > 0)
+                return true;
+            else return false;
+        }
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            txtMaTheLoai.Enabled = true;
+            txtMaTheLoai.Text = "";
+            txtTenTheLoai.Text = "";
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            load_theloai("", "");
+        }
+
+        private void btnThoat_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult tl = MessageBox.Show("Bạn có muốn xoá hay không?", "Detele Box", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (tl == DialogResult.Yes)
+            {
+                string ml = txtMaTheLoai.Text.Trim();
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string sql = "Delete theloai Where matheloai='" + ml + "'";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                con.Close();
+            }
+            load_theloai("", "");
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string ml = txtMaTheLoai.Text.Trim();
+            string tl = txtTenTheLoai.Text.Trim();
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "update theloai Set tentheloai=N'" + tl + "' where matheloai='" + ml + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+            MessageBox.Show("Sửa thành công!");
+            load_theloai("", "");
+
+        }
+
+        private void dgvCapNhatTheLoai_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex;
+            txtMaTheLoai.Text = dgvCapNhatTheLoai.Rows[i].Cells[0].Value.ToString();
+            txtTenTheLoai.Text = dgvCapNhatTheLoai.Rows[i].Cells[1].Value.ToString();
+            txtMaTheLoai.Enabled = false; //để không sửa được mã loại
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string ml = txtMaTheLoai.Text.Trim();
+            string tl = txtTenTheLoai.Text.Trim();
+            if (ml == "")
+            {
+                MessageBox.Show("Mã thể loại rỗng");
+                txtMaTheLoai.Focus();
+                return;
+            }
+            if (checktrungmatheloai(ml))
+            {
+                MessageBox.Show("Trùng mã thể loại");
+                txtMaTheLoai.Focus();
+                return;
+            }
+            if (tl == "")
+            {
+                MessageBox.Show("Tên thể loại không được rỗng!");
+                txtTenTheLoai.Focus();
+                return;
+            }
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "Insert theloai Values (N'" + ml + "',N'" + tl + "')";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+            MessageBox.Show("Thêm mới thành công");
+            load_theloai("", "");
+
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            
+        }
+
+        private void FormTheloai_Load(object sender, EventArgs e)
+        {
+            btnLuu.Enabled = false;
+            load_theloai("", "");
+        }
+
         private void btnThem_Click(object sender, EventArgs e)
         {
-            
+            btnLuu.Enabled=true;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            load_theloai("", "");
         }
     }
 }
+
+
+
+
+
