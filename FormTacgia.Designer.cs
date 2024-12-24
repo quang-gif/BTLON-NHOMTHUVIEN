a3a949f3451de2d475e8d1922a263adfb36184bb
PhamVanTan74 <tanphanchan@gmail.com>
2024-12-24 15:53:48 +0700

    Gửi form tác giả

--- FormTacgia.Designer.cs
+++ FormTacgia.Designer.cs
@@ -39,8 +39,9 @@
             this.btnLuu = new System.Windows.Forms.Button();
             this.btnThoat = new System.Windows.Forms.Button();
             this.dgvCapNhatTacGia = new System.Windows.Forms.DataGridView();
-            this.matacgia = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tentacgia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.matg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.httacgia = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCapNhatTacGia)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,10 +49,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(78, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(104, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(284, 31);
+            this.label1.Size = new System.Drawing.Size(340, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "CẬP NHẬT TÁC GIẢ";
             // 
@@ -59,10 +59,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 67);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(37, 82);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.Size = new System.Drawing.Size(112, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mã tác giả";
             // 
@@ -70,112 +69,131 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 110);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(37, 135);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.Size = new System.Drawing.Size(104, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Họ và tên";
             // 
             // txtMaTacGia
             // 
-            this.txtMaTacGia.Location = new System.Drawing.Point(118, 68);
-            this.txtMaTacGia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMaTacGia.Location = new System.Drawing.Point(157, 84);
+            this.txtMaTacGia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMaTacGia.Name = "txtMaTacGia";
-            this.txtMaTacGia.Size = new System.Drawing.Size(226, 20);
+            this.txtMaTacGia.Size = new System.Drawing.Size(300, 22);
             this.txtMaTacGia.TabIndex = 3;
             // 
             // txtHoTen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(118, 113);
-            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHoTen.Location = new System.Drawing.Point(157, 139);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtHoTen.Name = "txtHoTen";
-            this.txtHoTen.Size = new System.Drawing.Size(226, 20);
+            this.txtHoTen.Size = new System.Drawing.Size(300, 22);
             this.txtHoTen.TabIndex = 4;
             // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(362, 67);
-            this.btnThem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThem.Location = new System.Drawing.Point(483, 82);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(56, 19);
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
             this.btnThem.TabIndex = 5;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // btnSua
             // 
-            this.btnSua.Location = new System.Drawing.Point(362, 110);
-            this.btnSua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSua.Location = new System.Drawing.Point(483, 135);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(56, 19);
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
             this.btnSua.TabIndex = 6;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(362, 150);
-            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXoa.Location = new System.Drawing.Point(483, 185);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(56, 19);
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
             this.btnXoa.TabIndex = 7;
             this.btnXoa.Text = "Xoá";
             this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnLuu
             // 
-            this.btnLuu.Location = new System.Drawing.Point(362, 188);
-            this.btnLuu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLuu.Location = new System.Drawing.Point(483, 231);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(56, 19);
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
             this.btnLuu.TabIndex = 8;
             this.btnLuu.Text = "Lưu";
             this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(362, 228);
-            this.btnThoat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThoat.Location = new System.Drawing.Point(483, 315);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(56, 19);
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
             this.btnThoat.TabIndex = 9;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // dgvCapNhatTacGia
             // 
             this.dgvCapNhatTacGia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCapNhatTacGia.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.matacgia,
-            this.tentacgia});
-            this.dgvCapNhatTacGia.Location = new System.Drawing.Point(32, 150);
-            this.dgvCapNhatTacGia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.matg,
+            this.httacgia});
+            this.dgvCapNhatTacGia.Location = new System.Drawing.Point(43, 185);
+            this.dgvCapNhatTacGia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvCapNhatTacGia.Name = "dgvCapNhatTacGia";
             this.dgvCapNhatTacGia.RowHeadersWidth = 51;
             this.dgvCapNhatTacGia.RowTemplate.Height = 24;
-            this.dgvCapNhatTacGia.Size = new System.Drawing.Size(310, 226);
+            this.dgvCapNhatTacGia.Size = new System.Drawing.Size(413, 278);
             this.dgvCapNhatTacGia.TabIndex = 10;
+            this.dgvCapNhatTacGia.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCapNhatTacGia_CellClick);
             // 
-            // matacgia
+            // btnReset
             // 
-            this.matacgia.HeaderText = "Mã tác giả";
-            this.matacgia.MinimumWidth = 6;
-            this.matacgia.Name = "matacgia";
-            this.matacgia.Width = 125;
+            this.btnReset.Location = new System.Drawing.Point(483, 273);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // tentacgia
+            // matg
             // 
-            this.tentacgia.HeaderText = "Tên tác giả";
-            this.tentacgia.MinimumWidth = 6;
-            this.tentacgia.Name = "tentacgia";
-            this.tentacgia.Width = 125;
+            this.matg.DataPropertyName = "matg";
+            this.matg.HeaderText = "Mã tác giả";
+            this.matg.MinimumWidth = 6;
+            this.matg.Name = "matg";
+            this.matg.Width = 125;
+            // 
+            // httacgia
+            // 
+            this.httacgia.DataPropertyName = "httacgia";
+            this.httacgia.HeaderText = "Tên tác giả";
+            this.httacgia.MinimumWidth = 6;
+            this.httacgia.Name = "httacgia";
+            this.httacgia.Width = 125;
             // 
             // FormTacgia
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(440, 387);
+            this.ClientSize = new System.Drawing.Size(587, 476);
+            this.Controls.Add(this.btnReset);
             this.Controls.Add(this.dgvCapNhatTacGia);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnLuu);
@@ -187,9 +205,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormTacgia";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tác giả";
+            this.Load += new System.EventHandler(this.FormTacgia_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvCapNhatTacGia)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -209,7 +229,8 @@
         private System.Windows.Forms.Button btnLuu;
         private System.Windows.Forms.Button btnThoat;
         private System.Windows.Forms.DataGridView dgvCapNhatTacGia;
-        private System.Windows.Forms.DataGridViewTextBoxColumn matacgia;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tentacgia;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn matg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn httacgia;
     }
 }
--- FormTacgia.cs
+++ FormTacgia.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,150 @@ namespace BTLON_NHOMTHUVIEN
 {
     public partial class FormTacgia : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-T6775II7\\SQLEXPRESS;Initial Catalog=DUANNHOMTHUVIEN;Integrated Security=True;Encrypt=False");
         public FormTacgia()
         {
             InitializeComponent();
+        }
+        DataTable tb1;
+        private void load_tacgia(string mtg, string ttg)
+        {
+
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "select * from tacgia where " +
+                "matg like '%' +@matg+ '%' and " +
+                "httacgia like '%'+@httacgia+'%' ";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.Add("@matg", SqlDbType.NVarChar, 50).Value = mtg;
+            cmd.Parameters.Add("@httacgia", SqlDbType.NVarChar, 50).Value = ttg;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            tb1 = new DataTable();
+            da.Fill(tb1);
+            cmd.Dispose();
+            con.Close();
+            dgvCapNhatTacGia.DataSource = tb1;
+            dgvCapNhatTacGia.Refresh();
+        }
+        private bool checktrungmatg(string mtg)
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "select count(*) From tacgia where matg='" + mtg + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            int kq = int.Parse(cmd.ExecuteScalar().ToString());
+            if (kq > 0)
+                return true;
+            else return false;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMaTacGia.Enabled = true;
+            txtMaTacGia.Text = "";
+            txtHoTen.Text = "";
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            load_tacgia("", "");
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult tl = MessageBox.Show("Bạn có muốn xoá hay không?", "Detele Box", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (tl == DialogResult.Yes)
+            {
+                string mtg = txtMaTacGia.Text.Trim();
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string sql = "Delete tacgia Where matg='" + mtg + "'";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                con.Close();
+            }
+            load_tacgia("", "");
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string mtg = txtMaTacGia.Text.Trim();
+            string ttg = txtHoTen.Text.Trim();
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "update tacgia Set httacgia=N'" + ttg + "' where matg='" + mtg + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+            MessageBox.Show("Sửa thành công!");
+            load_tacgia("", "");
+        }
+
+        private void dgvCapNhatTacGia_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex;
+            txtMaTacGia.Text = dgvCapNhatTacGia.Rows[i].Cells[0].Value.ToString();
+            txtHoTen.Text = dgvCapNhatTacGia.Rows[i].Cells[1].Value.ToString();
+            txtMaTacGia.Enabled = false;
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string mtg = txtMaTacGia.Text.Trim();
+            string ttg = txtHoTen.Text.Trim();
+            if (mtg == "")
+            {
+                MessageBox.Show("Mã tác giả rỗng");
+                txtMaTacGia.Focus();
+                return;
+            }
+            if (checktrungmatg(mtg))
+            {
+                MessageBox.Show("Trùng mã tác giả");
+                txtMaTacGia.Focus();
+                return;
+            }
+            if (ttg == "")
+            {
+                MessageBox.Show("Tên tác giả không được rỗng!");
+                txtHoTen.Focus();
+                return;
+            }
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string sql = "Insert tacgia Values (N'" + mtg + "',N'" + ttg + "')";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+            MessageBox.Show("Thêm mới thành công");
+            load_tacgia("", "");
+
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+        }
+
+        private void FormTacgia_Load(object sender, EventArgs e)
+        {
+            btnLuu.Enabled = false;
+            load_tacgia("", "");
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            btnLuu.Enabled = true;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            load_tacgia("", "");
         }
     }
 }
