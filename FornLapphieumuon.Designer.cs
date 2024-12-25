fb0023a6824703891eaad5fe5bdf2794a29889be
Nguyen Anh Dung <Nguyen Anh Dung@SHIBAINU>
2024-12-25 23:01:54 +0700

    bố code xong r mấy con dog

--- FornLapphieumuon.Designer.cs
+++ FornLapphieumuon.Designer.cs
@@ -37,16 +37,13 @@
             this.cbbdocgia = new System.Windows.Forms.ComboBox();
             this.cbbnhanvien = new System.Windows.Forms.ComboBox();
             this.dgv1 = new System.Windows.Forms.DataGridView();
-            this.maphieumuon = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tensach = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.masach = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.madocgia = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.manhanvien = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mamuon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.madocgia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.manhanvien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnreset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,12 +54,14 @@
             this.btnthem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnthem.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnthem.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnthem.Location = new System.Drawing.Point(707, 30);
+            this.btnthem.Location = new System.Drawing.Point(858, 24);
+            this.btnthem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnthem.Name = "btnthem";
-            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.Size = new System.Drawing.Size(112, 35);
             this.btnthem.TabIndex = 1;
             this.btnthem.Text = "Thêm";
             this.btnthem.UseVisualStyleBackColor = false;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
             this.btnthem.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
             // 
             // btnsua
@@ -72,12 +71,14 @@
             this.btnsua.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnsua.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnsua.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnsua.Location = new System.Drawing.Point(392, 138);
+            this.btnsua.Location = new System.Drawing.Point(552, 257);
+            this.btnsua.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnsua.Name = "btnsua";
-            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.Size = new System.Drawing.Size(112, 35);
             this.btnsua.TabIndex = 1;
             this.btnsua.Text = "Sửa";
             this.btnsua.UseVisualStyleBackColor = false;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
             this.btnsua.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
             // 
             // btnluu
@@ -87,12 +88,14 @@
             this.btnluu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnluu.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnluu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnluu.Location = new System.Drawing.Point(246, 138);
+            this.btnluu.Location = new System.Drawing.Point(393, 257);
+            this.btnluu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnluu.Name = "btnluu";
-            this.btnluu.Size = new System.Drawing.Size(75, 23);
+            this.btnluu.Size = new System.Drawing.Size(112, 35);
             this.btnluu.TabIndex = 1;
             this.btnluu.Text = "Lưu";
             this.btnluu.UseVisualStyleBackColor = false;
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click);
             this.btnluu.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
             // 
             // btnthoat
@@ -102,12 +105,14 @@
             this.btnthoat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnthoat.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnthoat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnthoat.Location = new System.Drawing.Point(536, 138);
+            this.btnthoat.Location = new System.Drawing.Point(858, 155);
+            this.btnthoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnthoat.Name = "btnthoat";
-            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.Size = new System.Drawing.Size(112, 35);
             this.btnthoat.TabIndex = 1;
             this.btnthoat.Text = "Thoát";
             this.btnthoat.UseVisualStyleBackColor = false;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
             this.btnthoat.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
             // 
             // btnxoa
@@ -117,95 +122,68 @@
             this.btnxoa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnxoa.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnxoa.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnxoa.Location = new System.Drawing.Point(707, 76);
+            this.btnxoa.Location = new System.Drawing.Point(858, 85);
+            this.btnxoa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnxoa.Name = "btnxoa";
-            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.Size = new System.Drawing.Size(112, 35);
             this.btnxoa.TabIndex = 1;
             this.btnxoa.Text = "Xóa";
             this.btnxoa.UseVisualStyleBackColor = false;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
             this.btnxoa.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
             // 
             // txtphieumuon
             // 
-            this.txtphieumuon.Location = new System.Drawing.Point(190, 30);
+            this.txtphieumuon.Location = new System.Drawing.Point(443, 24);
+            this.txtphieumuon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtphieumuon.Multiline = true;
             this.txtphieumuon.Name = "txtphieumuon";
-            this.txtphieumuon.Size = new System.Drawing.Size(159, 22);
+            this.txtphieumuon.Size = new System.Drawing.Size(236, 32);
             this.txtphieumuon.TabIndex = 2;
             // 
             // cbbdocgia
             // 
             this.cbbdocgia.FormattingEnabled = true;
-            this.cbbdocgia.Location = new System.Drawing.Point(190, 75);
+            this.cbbdocgia.Location = new System.Drawing.Point(443, 159);
+            this.cbbdocgia.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbbdocgia.Name = "cbbdocgia";
-            this.cbbdocgia.Size = new System.Drawing.Size(171, 21);
+            this.cbbdocgia.Size = new System.Drawing.Size(254, 28);
             this.cbbdocgia.TabIndex = 3;
             // 
             // cbbnhanvien
             // 
             this.cbbnhanvien.FormattingEnabled = true;
-            this.cbbnhanvien.Location = new System.Drawing.Point(507, 73);
+            this.cbbnhanvien.Location = new System.Drawing.Point(443, 89);
+            this.cbbnhanvien.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbbnhanvien.Name = "cbbnhanvien";
-            this.cbbnhanvien.Size = new System.Drawing.Size(171, 21);
+            this.cbbnhanvien.Size = new System.Drawing.Size(254, 28);
             this.cbbnhanvien.TabIndex = 3;
             // 
             // dgv1
             // 
+            this.dgv1.AllowUserToAddRows = false;
             this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.maphieumuon,
-            this.tensach,
-            this.masach,
+            this.mamuon,
             this.madocgia,
             this.manhanvien});
-            this.dgv1.Location = new System.Drawing.Point(49, 196);
+            this.dgv1.Location = new System.Drawing.Point(74, 302);
+            this.dgv1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersVisible = false;
             this.dgv1.RowHeadersWidth = 62;
-            this.dgv1.Size = new System.Drawing.Size(704, 231);
+            this.dgv1.Size = new System.Drawing.Size(1056, 355);
             this.dgv1.TabIndex = 4;
-            // 
-            // maphieumuon
-            // 
-            this.maphieumuon.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.maphieumuon.HeaderText = "Mã phiếu ";
-            this.maphieumuon.MinimumWidth = 8;
-            this.maphieumuon.Name = "maphieumuon";
-            // 
-            // tensach
-            // 
-            this.tensach.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.tensach.HeaderText = "Tên sách";
-            this.tensach.MinimumWidth = 8;
-            this.tensach.Name = "tensach";
-            // 
-            // masach
-            // 
-            this.masach.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.masach.HeaderText = "Mã sách";
-            this.masach.MinimumWidth = 8;
-            this.masach.Name = "masach";
-            // 
-            // madocgia
-            // 
-            this.madocgia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.madocgia.HeaderText = "Mã độc giả";
-            this.madocgia.MinimumWidth = 8;
-            this.madocgia.Name = "madocgia";
-            // 
-            // manhanvien
-            // 
-            this.manhanvien.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.manhanvien.HeaderText = "Mã nhân viên";
-            this.manhanvien.MinimumWidth = 8;
-            this.manhanvien.Name = "manhanvien";
+            this.dgv1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(33, 30);
+            this.label1.Location = new System.Drawing.Point(230, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.Size = new System.Drawing.Size(178, 29);
             this.label1.TabIndex = 5;
             this.label1.Text = "Mã phiếu mượn";
             // 
@@ -213,9 +191,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 76);
+            this.label2.Location = new System.Drawing.Point(230, 159);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.Size = new System.Drawing.Size(131, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "Mã độc giả";
             // 
@@ -223,39 +202,53 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(389, 72);
+            this.label3.Location = new System.Drawing.Point(230, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.Size = new System.Drawing.Size(154, 29);
             this.label3.TabIndex = 5;
             this.label3.Text = "Mã nhân viên";
             // 
-            // label4
+            // mamuon
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(389, 30);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 20);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Mã sách";
+            this.mamuon.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.mamuon.DataPropertyName = "mamuon";
+            this.mamuon.HeaderText = "Mã phiếu ";
+            this.mamuon.MinimumWidth = 8;
+            this.mamuon.Name = "mamuon";
             // 
-            // textBox1
+            // madocgia
             // 
-            this.textBox1.Location = new System.Drawing.Point(507, 30);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(152, 22);
-            this.textBox1.TabIndex = 7;
+            this.madocgia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.madocgia.DataPropertyName = "madg";
+            this.madocgia.HeaderText = "Mã độc giả";
+            this.madocgia.MinimumWidth = 8;
+            this.madocgia.Name = "madocgia";
+            // 
+            // manhanvien
+            // 
+            this.manhanvien.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.manhanvien.DataPropertyName = "manhanvien";
+            this.manhanvien.HeaderText = "Mã nhân viên";
+            this.manhanvien.MinimumWidth = 8;
+            this.manhanvien.Name = "manhanvien";
+            // 
+            // btnreset
+            // 
+            this.btnreset.Location = new System.Drawing.Point(717, 257);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(85, 35);
+            this.btnreset.TabIndex = 6;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = true;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
             // 
             // FornLapphieumuon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnreset);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -268,9 +261,11 @@
             this.Controls.Add(this.btnluu);
             this.Controls.Add(this.btnsua);
             this.Controls.Add(this.btnthem);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FornLapphieumuon";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lập phiếu mượn";
+            this.Load += new System.EventHandler(this.FornLapphieumuon_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -290,12 +285,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn maphieumuon;
-        private System.Windows.Forms.DataGridViewTextBoxColumn tensach;
-        private System.Windows.Forms.DataGridViewTextBoxColumn masach;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mamuon;
         private System.Windows.Forms.DataGridViewTextBoxColumn madocgia;
         private System.Windows.Forms.DataGridViewTextBoxColumn manhanvien;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnreset;
     }
 }
--- FornLapphieumuon.cs
+++ FornLapphieumuon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,85 @@ namespace BTLON_NHOMTHUVIEN
 {
     public partial class FornLapphieumuon : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=ShibaInu\\SQLEXPRESS01;Initial Catalog=ThuVien;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
+ 
         public FornLapphieumuon()
         {
             InitializeComponent();
+        }
+
+        private void load_docgia()
+        {
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+
+            string sql = "select * from docgia ";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter ad = new SqlDataAdapter();
+            ad.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            cmd.Dispose();
+            con.Close();
+            
+            DataRow r = dt.NewRow();
+            
+            r["madg"] = "---Chọn mã độc giả---";
+            dt.Rows.InsertAt(r, 0);
+
+            cbbdocgia.DataSource = dt;
+            cbbdocgia.DisplayMember = "madg";
+            cbbdocgia.ValueMember = "madg";
+        }
+
+        private void load_nhanvien()
+        {
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+
+            string sql = "select * from nhanvien ";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter ad = new SqlDataAdapter();
+            ad.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            cmd.Dispose();
+            con.Close();
+
+            DataRow r = dt.NewRow();
+            r["manhanvien"] = "---Chọn mã nhân viên---";
+            dt.Rows.InsertAt(r, 0);
+
+            cbbnhanvien.DataSource = dt;
+            cbbnhanvien.DisplayMember = "manhanvien";
+            cbbnhanvien.ValueMember = "manhanvien";
+        }
+
+        private void load_muonsach()
+        {
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+
+            string sql = "select * from muontra";
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cmd.Dispose();
+            con.Close();
+
+            dgv1.DataSource = dt;
+            dgv1.Refresh();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,6 +107,143 @@ namespace BTLON_NHOMTHUVIEN
         private void button3_MouseEnter(object sender, EventArgs e)
         {
 
+        }
+
+        private void FornLapphieumuon_Load(object sender, EventArgs e)
+        {
+            btnluu.Enabled = false;
+            load_docgia();
+            load_nhanvien();
+            load_muonsach();
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+            //b1: lay du lieu tu cac control dua vao bien
+            string pm = txtphieumuon.Text.Trim();
+            string nv = cbbnhanvien.SelectedValue.ToString();
+            string dg = cbbdocgia.SelectedValue.ToString();
+            
+
+            //b2: ket noi db
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            if (pm == "")
+            {
+                MessageBox.Show("Không được để trống thông tin! ");
+                txtphieumuon.Focus();
+                return;
+            }
+
+
+            if (dg == "---Chọn mã độc giả---")
+            {
+                MessageBox.Show("Không được để trống thông tin! ");
+                txtphieumuon.Focus();
+                return;
+            }
+
+            if (nv == "---Chọn mã nhân viên---")
+            {
+                MessageBox.Show("Không được để trống thông tin! ");
+                txtphieumuon.Focus();
+                return;
+            }
+
+            //b3: tao doi tuong command de thuc hien cau lenh sql
+            string sql = "insert muontra values (@mamuon, @madg, @manhanvien)";
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            //b4
+            cmd.Parameters.Add("@mamuon", SqlDbType.NVarChar, 50).Value = pm;
+            cmd.Parameters.Add("@manhanvien", SqlDbType.NVarChar, 50).Value = nv;
+            cmd.Parameters.Add("@madg", SqlDbType.NVarChar, 50).Value = dg;
+            
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+
+            MessageBox.Show("Lưu mới thành công!");
+            load_muonsach();
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            string pm = txtphieumuon.Text.Trim();
+            string nv = cbbnhanvien.SelectedValue.ToString();
+            string dg = cbbdocgia.SelectedValue.ToString();
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            if (string.IsNullOrEmpty(pm)) 
+            { 
+                MessageBox.Show("Vui lòng chọn thông tin sửa!" , "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string sql = "Update muontra set manhanvien = '" + nv + "', madg =  '"+dg+"' where mamuon = '"+pm+"'";
+            SqlCommand cmd = new SqlCommand(@sql, con);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+
+            MessageBox.Show("Sửa thành công!");
+            load_muonsach();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            btnluu.Enabled = true;
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            DialogResult tl = MessageBox.Show("Bạn có muốn xoá hay không?", "Detele Box", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (tl == DialogResult.Yes)
+            {
+                
+                string pm = txtphieumuon.Text.Trim();
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string sql = "Delete muontra Where mamuon ='" + pm + "'";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                con.Close();
+            }
+            load_muonsach();
+
+        }
+
+
+
+        private void dgv1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex;
+           
+            txtphieumuon.Text = dgv1.Rows[i].Cells[0].Value.ToString();
+            cbbdocgia.SelectedValue = dgv1.Rows[i].Cells[1].Value.ToString();
+            cbbnhanvien.SelectedValue = dgv1.Rows[i].Cells[2].Value.ToString();
+            txtphieumuon.Enabled = false;
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            txtphieumuon.Enabled = true;
+            txtphieumuon.Text = "";
+            load_docgia();
+            load_nhanvien();
         }
     }
 }
