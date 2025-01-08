631545803ba33b74c2567e56e06882b522a1afde
LongGey <ACER@LAPTOP-F4RS79DJ>
2025-01-08 22:31:02 +0700

    Chỉnh Excel BCsach

--- FormBCsach.cs
+++ FormBCsach.cs
@@ -92,12 +92,14 @@ namespace BTLON_NHOMTHUVIEN
 
         private void ExportExcel(DataTable tb, string sheetname)
         {
+            //Tạo các đối tượng Excel
             e_excel.Application oExcel = new e_excel.Application();
             e_excel.Workbooks oBooks;
             e_excel.Sheets oSheets;
             e_excel.Workbook oBook;
             e_excel.Worksheet oSheet;
 
+            //Tạo mới một Excel WorkBook 
             oExcel.Visible = true;
             oExcel.DisplayAlerts = false;
             oExcel.Application.SheetsInNewWorkbook = 1;
@@ -107,7 +109,8 @@ namespace BTLON_NHOMTHUVIEN
             oSheet = (e_excel.Worksheet)oSheets.get_Item(1);
             oSheet.Name = sheetname;
 
-            e_excel.Range head = oSheet.get_Range("A1", "E1");
+            //Tiêu đề bảng
+            e_excel.Range head = oSheet.get_Range("A1", "G1");
             head.MergeCells = true;
             head.Value2 = "BÁO CÁO THỐNG KÊ SÁCH TRONG THƯ VIỆN";
             head.Font.Bold = true;
@@ -115,6 +118,7 @@ namespace BTLON_NHOMTHUVIEN
             head.Font.Size = "16";
             head.HorizontalAlignment = e_excel.XlHAlign.xlHAlignCenter;
 
+            //Tiêu đề cột
             e_excel.Range cl1 = oSheet.get_Range("A3", "A3");
             cl1.Value2 = "MÃ SÁCH";
             cl1.ColumnWidth = 15;
@@ -122,46 +126,69 @@ namespace BTLON_NHOMTHUVIEN
             cl2.Value2 = "TÊN SÁCH";
             cl2.ColumnWidth = 25.0;
             e_excel.Range cl3 = oSheet.get_Range("C3", "C3");
-            cl3.Value2 = "NXB";
-            cl3.ColumnWidth = 15.0;
+            cl3.Value2 = "NĂM XB";
+            cl3.ColumnWidth = 25.0;
             e_excel.Range cl4 = oSheet.get_Range("D3", "D3");
-            cl4.Value2 = "NĂM XB";
+            cl4.Value2 = "NXB";
             cl4.ColumnWidth = 15.0;
             e_excel.Range cl5 = oSheet.get_Range("E3", "E3");
             cl5.Value2 = "THÊ LOẠI";
             cl5.ColumnWidth = 15.0;
-            e_excel.Range rowHead = oSheet.get_Range("A3", "E3");
+            e_excel.Range cl6 = oSheet.get_Range("F3", "F3");
+            cl6.Value2 = "NGÀY MƯỢN";
+            cl6.ColumnWidth = 25.0;
+            e_excel.Range cl7 = oSheet.get_Range("G3", "G3");
+            cl7.Value2 = "NGÀY TRẢ";
+            cl7.ColumnWidth = 25.0;
+            e_excel.Range rowHead = oSheet.get_Range("A3", "G3");
             rowHead.Font.Bold = true;
+
+            // Kẻ viền
             rowHead.Borders.LineStyle = e_excel.Constants.xlSolid;
+
+            // Thiết lập màu nền
             rowHead.Interior.ColorIndex = 15;
             rowHead.HorizontalAlignment = e_excel.XlHAlign.xlHAlignCenter;
+            // Tạo mảng đối tượng để lưu dữ toàn bồ dữ liệu trong DataTable,
+            // vì dữ liệu được được gán vào các Cell trong Excel phải thông qua object thuần.
             object[,] arr = new object[tb.Rows.Count, tb.Columns.Count];
+            //Chuyển dữ liệu từ DataTable vào mảng đối tượng
             for (int r = 0; r < tb.Rows.Count; r++)
             {
                 DataRow dr = tb.Rows[r];
                 for (int c = 0; c < tb.Columns.Count; c++)
 
                 {
-                    if (c == 2)
-                        arr[r, c] = "'" + dr[c].ToString();
+                    if (c == 2 || c == 5 || c == 6)
+                        arr[r, c] = "'" + dr[c].ToString(); //chuyen cot C thanh Text
                     else
                         arr[r, c] = dr[c];
                 }
             }
+            //Thiết lập vùng điền dữ liệu
             int rowStart = 4;
             int columnStart = 1;
             int rowEnd = rowStart + tb.Rows.Count - 1;
             int columnEnd = tb.Columns.Count;
+            // Ô bắt đầu điền dữ liệu
             e_excel.Range c1 = (e_excel.Range)oSheet.Cells[rowStart, columnStart];
+            // Ô kết thúc điền dữ liệu
             e_excel.Range c2 = (e_excel.Range)oSheet.Cells[rowEnd, columnEnd];
+            // Lấy về vùng điền dữ liệu
             e_excel.Range range = oSheet.get_Range(c1, c2);
+            //Điền dữ liệu vào vùng đã thiết lập
             range.Value2 = arr;
+            // Kẻ viền
             range.Borders.LineStyle = e_excel.Constants.xlSolid;
+            // Căn giữa cột STT
             e_excel.Range c3 = (e_excel.Range)oSheet.Cells[rowEnd, columnStart];
             e_excel.Range c4 = oSheet.get_Range(c1, c3);
             oSheet.get_Range(c3, c4).HorizontalAlignment = e_excel.XlHAlign.xlHAlignCenter;
+            //dinh dang ngay thang cot C
             e_excel.Range cotBD = (e_excel.Range)oSheet.Cells[rowStart, columnStart];
             e_excel.Range cotKT = (e_excel.Range)oSheet.Cells[rowEnd, columnEnd];
+            //
+            //
         }
 
         private void btExcel_Click(object sender, EventArgs e)
